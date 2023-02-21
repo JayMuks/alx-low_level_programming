7cdaa077807bb0b4870d478cef545e137fbee60d
JayMuks <jacintamukui20@gmail.com>
2023-02-21 11:30:16 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+		while (*sh)
+		{
+			_putchar(*sh);
+			sh++;
+			'}
+		_putchar('\n');
+
+		return (0);
+}
