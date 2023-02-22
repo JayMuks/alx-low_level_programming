cb08d7fc0d63f180367e9696ec244a13027b467f
JayMuks <jacintamukui20@gmail.com>
2023-02-22 04:56:46 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
-#include "_putchar"
+#include "main.h"
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ * main - Entry point
+ * Return: Always 0
+ */
+
 int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,6 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
